a843d55958654e171cdb74ada7aa534fe3b24495
Dario Campagna <Dario Campagna>
2020-06-09 08:37:12 -0300

    Utilizando o comando console.log(), com node js e code runner.

---
+++ modulo02-basico/aula03/exemplo_consolelog.js
@@ -0,0 +1,9 @@
+/*
+  Outro exemplo usando o console log, utilizando o node js mais a extensão code runner.
+  Executado no Visual Studio Code. 
+*/
+
+//Texto
+console.log("Dario Campagna")
+//Número
+console.log(10);
