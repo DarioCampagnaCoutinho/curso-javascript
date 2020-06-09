8301704cf2bac5e91b8af0715c20624164e85374
Dario Campagna <Dario Campagna>
2020-06-09 09:16:43 -0300

    Utilizando let, com node js e code runner.

---
+++ modulo02-basico/aula04/exemplo_let01.js
@@ -0,0 +1,15 @@
+// Não podemos criar variáveis com palavras reservadas
+// Variáveis precisam ter nomes significativos
+// Não pode começar o nome de uma variável com um número
+// Não podem conter espaços ou traços
+// Utilizamos camelCase
+// Case-sensitive
+// Não podemos redeclarar variáveis com let
+// NÃO UTILIZE VAR, UTILIZE LET.
+
+let nome = 'Dario';
+let idade = 32;
+
+console.log(nome, 'nasceu em 1985.')
+console.log(nome, 'mora em Berlin.')
+console.log(nome, 'atualmente mora em Amsterdam, e sua idade é ', idade, 'anos.')
