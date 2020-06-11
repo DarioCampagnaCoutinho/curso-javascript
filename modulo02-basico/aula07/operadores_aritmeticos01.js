0a43a38984602e813c4641b5821d31268ec0dd8a
Dario Campagna <Dario Campagna>
2020-06-11 09:50:29 -0300

    Operadores Aritmeticos

---
+++ modulo02-basico/aula07/operadores_aritmeticos01.js
@@ -0,0 +1,18 @@
+/**
+ * Operadores Aritméticos
+ * + adição
+ * - subtração
+ * / divisão
+ * * multiplicação
+ * ** potênciação
+ * % resto da divisão
+ */
+
+let numero1 = 15;
+let numero2 = 6;
+console.log('Adição = ', numero1 + numero2);
+console.log('Subtração = ', numero1 - numero2);
+console.log('Divisão = ', numero1 / numero2);
+console.log('Multiplicação = ', numero1 * numero2);
+console.log('Potênciação = ', numero1 ** numero2);
+console.log('Resto da divisão = ', numero1 % numero2);
