3ba2e9a76a414e116aeac77bac49e07f0f906ad4
Dario Campagna <Dario Campagna>
2020-06-11 09:32:31 -0300

    Tipos Primitivos

---
+++ modulo02-basico/aula06/tipos_primitivos01.js
@@ -0,0 +1,19 @@
+
+//Tipos primitivos
+// String, number, undefined, null, boolean
+const nome = 'Dario'; // string
+const sobreNome = 'Campagna'; // string
+const fruta = 'Jaca'; // string
+const numero1 = 30; // number
+const numero2 = 50.23; // number
+let nomeAluno; // undefined -> não aponta pra local nenhuma na memória
+const sobreNomeAluno = null; // Nulo -> não aponta pra local nenhuma na memória
+const reprovado = false; // Boolean = true, false (lógico)
+
+
+let a = 2;
+const b = a;
+console.log(a, b); // 2, 2
+
+a = 3;
+console.log(a, b); // 3, 2
