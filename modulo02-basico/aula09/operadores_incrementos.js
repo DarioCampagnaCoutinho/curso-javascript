9e81c04a3ab612f821d4640218a03ee8487d9f46
Dario Campagna <campagnacoutinho67@gmail.com>
2020-06-14 11:07:14 -0300

    Operadores de Incremento

---
+++ modulo02-basico/aula09/operadores_incrementos.js
@@ -0,0 +1,15 @@
+let contadorSoma = 7;
+contadorSoma++;
+console.log('Soma mais um :', contadorSoma)
+
+let contadorSubtracao = 9;
+contadorSubtracao--;
+console.log('Subtrai um : ', contadorSubtracao);
+
+let contadorMultiplicacao = 7;
+contadorMultiplicacao = contadorMultiplicacao * 2;
+console.log('Multiplica dois : ', contadorMultiplicacao);
+
+let contadorDivisao = 12;
+contadorDivisao = contadorDivisao / 2;
+console.log('Dividi por dois : ', contadorDivisao);
