a6d3f08d181bcabe4b08ec47f8611910197ec18f
Dario Campagna <campagnacoutinho67@gmail.com>
2020-06-14 10:32:58 -0300

    Operadores Logicos

---
+++ modulo02-basico/aula08/operadores_logicos.js
@@ -0,0 +1,21 @@
+/**
+ * Operadores Aritméticos
+ * > maior
+ * < menor
+ * >= maior igual
+ * <= menor igual
+ * == igual
+ * != diferente
+ * === igual do mesmo tipo
+ */
+
+let numero1 = 15;
+let numero2 = 6;
+console.log('Maior = ', numero1 > numero2);
+console.log('Menor = ', numero1 < numero2);
+console.log('Maior igual = ', numero1 >= numero2);
+console.log('Menor igual = ', numero1 <= numero2);
+console.log('Igual = ', numero1 == numero2);
+console.log('Diferente = ', numero1 != numero2);
+console.log('Igual do mesmo tipo = ', numero1 === numero2);
+console.log('Diferente tipo = ', numero1 !== numero2);
