1c813fd30b1f56078dd4a3f743b1ec07ddf1dcd1
Dario Campagna <campagnacoutinho67@gmail.com>
2020-06-18 08:19:36 -0300

    Utilizando strings

---
+++ modulo02-basico/aula12/exemplo_string01.js
@@ -0,0 +1,14 @@
+let umaString = "Um texto";
+
+console.log(umaString[3]);
+console.log(umaString.charAt(4));
+console.log(umaString.charCodeAt(5));
+console.log(umaString.concat(' em um.'));
+console.log(umaString.indexOf('t'));
+console.log(umaString.lastIndexOf('o', 3));
+console.log(umaString.replace('Um', 'Outro'));
+console.log(umaString.length);
+console.log(umaString.slice(2, 5));
+console.log(umaString.split(' '));
+console.log(umaString.toLocaleUpperCase());
+console.log(umaString.toLocaleLowerCase());
