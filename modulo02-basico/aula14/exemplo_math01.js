784abdf99cd6bb95c13d7ac56c78c386b413bca3
Dario Campagna <campagnacoutinho67@gmail.com>
2020-06-19 10:52:35 -0300

    Objeto Math

---
+++ modulo02-basico/aula14/exemplo_math01.js
@@ -0,0 +1,14 @@
+let num1 = 9.5678;
+let num2 = 9.56432;
+let num3 = 9.4567;
+
+console.log(Math.floor(num1));
+console.log(Math.ceil(num2));
+console.log(Math.round(num1));
+console.log(Math.sqrt(num3));
+console.log(Math.pow(num1, 2));
+console.log(Math.max(1, 2, 34, 35, 23));
+console.log(Math.min(1, 2, 34, 35, 23));
+
+const aleatorio = Math.random() * (10 - 5) + 5;
+console.log(aleatorio)
