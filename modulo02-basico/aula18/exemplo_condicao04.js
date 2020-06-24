393bb1faba72f3699633b52bdd96532748a0598f
Dario Campagna <campagnacoutinho67@gmail.com>
2020-06-24 08:37:09 -0300

    If, else if, else e operador and.

---
+++ modulo02-basico/aula18/exemplo_condicao04.js
@@ -0,0 +1,10 @@
+let = numero = 6;
+
+if (numero >= 0 && numero <= 5) {
+    console.log('O número está entre 0 e 5 !!')
+} else if (numero >= 6 && numero <= 10) {
+    console.log('O número está entre 6 e 10 !!');
+}
+else {
+    console.log('O número não está entre em nenhuma sequência!!!')
+}
